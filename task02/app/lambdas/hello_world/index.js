e17a4e0d01f3c61469211f2bd2830a54642bcf7b
yktejCantire <krishna.yaramati@cantire.com>
2024-04-27 23:56:56 +0530

    task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        message: JSON.stringify('Hello from Lambda'),
+        body: JSON.stringify('Hello from Lambda'),
     };
     return response;
 };
